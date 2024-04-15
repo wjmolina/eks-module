2a020a9962ea5d5ba2164e654ce3d8abe8975b79
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2024-04-15 15:59:29 -0700

    add another output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
   value       = module.eks.cluster_oidc_issuer_url
 }
+
+output "oidc_provider" {
+  description = "The OpenID Connect identity provider (issuer URL without leading `https://`)"
+  value       = module.eks.oidc_provider
+}
